aa1912d15c43ce4b65e8dc33d4574ec104659315
alessanderleite <alessander.logistica@gmail.com>
2019-07-09 22:17:26 -0300

    update the application class name

--- src/main/java/br/com/alessanderleite/api/SpringbootcrudrestApplication.java
+++ src/main/java/br/com/alessanderleite/api/SpringbootcrudrestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootcrudrestApplication {
+public class SpringBootCrudRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootcrudrestApplication.class, args);
+		SpringApplication.run(SpringBootCrudRestApplication.class, args);
 	}
 
 }
